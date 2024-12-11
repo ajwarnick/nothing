08799ba1ff316e6a98076f228706fed16971646b
Anthony Warnick <github@warnick.co>
2024-12-10 18:22:28 -0600

    now with scss variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import node from '@astrojs/node';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
