b30cea4d3fc2d0232556da51eabcbf345401926e
Anthony Warnick <github@warnick.co>
2024-12-18 16:42:49 -0500

    Final Astro

--- src/components/BlurImage.jsx
+++ src/components/BlurImage.jsx
@@ -3,19 +3,20 @@ import { BlurhashCanvas } from "react-blurhash";
 
 function BlurImage() {
 
+  let hash = "LEHV6nWB2yk8pyo0adR*.7kCMdnj";
+      hash = "L5jsl0k8pyo0adR*QfQfQQfQfQfQ";
+
     return (
       <div className="BlurImage">
-        <img src="https://picsum.photos/900/700" alt=""></img>
         {/* <Blurhash
             hash="LEHV6nWB2yk8pyo0adR*.7kCMdnj"
             width={900}
             height={700}
-            resolutionX={100}
-            resolutionY={100}
+            resolutionX={10}
+            resolutionY={10}
             punch={0}
-        />
-        hi<br/>
-        <BlurhashCanvas hash="LEHV6nWB2yk8pyo0adR*.7kCMdnj" width={400} height={300} punch={1} /> */}
+        /> */}
+        <BlurhashCanvas hash={hash} width={900} height={700} punch={1} style={{border: "0px solid red", maxWidth: "100%"}} />
       </div>
     );
   }
