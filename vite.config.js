5585a3622cb4bf95044a90ef23addce77865ec3f
Anthony Warnick <github@warnick.co>
2025-01-17 20:02:45 -0600

    deploy working and now with light and dark themes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from "vite-plugin-svgr";
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), vercel()],
+  root: './',
+    build: {
+        outDir: 'dist',
+    },
+    publicDir: 'assets'
 })
