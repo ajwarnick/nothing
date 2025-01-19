1a198cbdf594a90d880d7ea55ed994a03421f448
Anthony Warnick <github@warnick.co>
2025-01-19 17:21:26 -0600

    comments started

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -4,13 +4,16 @@ import './Comments.scss';
 
 function Comments() {
 
+    // add class to body to frieze it
+    // add class to post__comments_comments to expand it
+
     return (
         <div className="post__comments">
             <Comment />
             <div>posted min</div>
             <div>View all 5 Comments</div>
             <div className='post__comments_comments'>
-                
+
             </div>
         </div>
         );
