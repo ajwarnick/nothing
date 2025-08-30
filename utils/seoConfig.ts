942d167c14392d46719822919a8ef52d9c21f654
Anthony Warnick <github@warnick.co>
2025-08-30 15:00:53 -0500

    update seo config

--- utils/seoConfig.ts
+++ utils/seoConfig.ts
@@ -5,17 +5,17 @@ import type { ManifestOptions } from "vite-plugin-pwa"
  * Defines the default SEO configuration for the website.
  */
 export const seoConfig = {
-	baseURL: "https://example.com", // Change this to your production URL.
+	baseURL: "https://void.anthonywarnick.com", // Change this to your production URL.
 	description:
-		"Astro PWA Starter is an opionated Astro starter for building robust static websites.", // Change this to be your website's description.
+		"Void - asocial network", // Change this to be your website's description.
 	type: "website",
 	image: {
-		url: "https://picsum.photos/1200/630", // Change this to your website's thumbnail.
-		alt: "OpenGraph thumbnail description.", // Change this to your website's thumbnail description.
-		width: 1200,
-		height: 630
+		url: "public/light-icons/icon-512x512.png", // Change this to your website's thumbnail.
+		alt: "Void logo", // Change this to your website's thumbnail description.
+		width: 512,
+		height: 512
 	},
-	siteName: "Astro PWA Starter", // Change this to your website's name,
+	siteName: "Void", // Change this to your website's name,
 	twitter: {
 		card: "summary_large_image"
 	}
