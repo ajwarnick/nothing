295e7534303bd8ad0df366b2babaee7910746ff5
Anthony Warnick <github@warnick.co>
2025-09-14 17:53:27 -0500

    almost ready for first submittion to app store

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'void.app',
   appName: 'void',
-  webDir: 'dist/client'
+  webDir: 'dist/client',
+  server: {
+    "androidScheme": "https",
+    "url": "http://localhost:4321/",
+    "cleartext": true
+  }
 };
 
 export default config;
